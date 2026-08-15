11e6766f70a4e9bde6f67fed5c0a37a37f60a118
SheteArnav <arnavshete07@gmail.com>
2026-08-15 23:49:51 +0530

    Add files via upload

---
+++ C++/ifelse.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num = 10;
+    if (num>0)
+     cout<<"positive number\n";
+   
+    
+  int marks = 80;
+    
+   if (marks>=90)
+     cout<<"Grade A";
+
+    else if (marks>=70)
+     cout<<"Grade B";
+    
+    else
+
+      cout<<"Fail";
+    
+
+ return 0;
+}
