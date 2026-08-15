305ec053af5563d483517c4e6a6fbf6094bc6fe0
SheteArnav <arnavshete07@gmail.com>
2026-08-16 00:19:00 +0530

    Update ifelse.cpp

--- C++/ifelse.cpp
+++ C++/ifelse.cpp
@@ -19,6 +19,23 @@ int main()
 
       cout<<"Fail";
     
+ int amount = 10;
+ int balance = 100;
+ int daily_limit = 50;
+
+  if(amount<=balance)
+      if(amount<=daily_limit)
+         if(amount%5==0)
+         cout<<"Withdrawl succesfully\n";
+        else
+          cout<<"Amount must be multiple of 100\n";
+     else
+     cout<<"Exceeds daily limit\n";
+ else
+      cout<<"Insufficient Balance\n";          
+
+ return 0;
+}
 
  return 0;
 }
